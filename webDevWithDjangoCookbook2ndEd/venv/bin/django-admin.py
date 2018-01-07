0b66468bc6da7bc69fb9d2a8f5aa606b1c3a8bc8
Kevin Harper <kevinharperg@gmail.com>
2018-01-06 22:27:29 -0700

    Added .gitignore and moved create folder for virtual environments

--- webDevWithDjangoCookbook2ndEd/venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/media/lex/Storage/Development/Python/PyCharm/classroom/webDevWithDjangoCookbook2ndEd/venv/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
