cecf5a2dd84fcd17cf6bf4857c21085b11f4fd1f
Kevin Harper <kevinharperg@gmail.com>
2018-09-17 09:22:55 -0700

    Added API views

--- buildingresfulpythonwebservices/chap01/Django01-pg17/gamesapi/games/urls.py
+++ buildingresfulpythonwebservices/chap01/Django01-pg17/gamesapi/games/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path
+from django.conf.urls import url
 
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    url(r'^games/$', views.game_list),
+    url(r'^games/(?P<pk>[0-9]+)/$', views.game_detail),
 ]
