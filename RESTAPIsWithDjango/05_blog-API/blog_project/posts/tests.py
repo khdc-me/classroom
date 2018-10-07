19c1079607c116e33f19b6fc2f50f97ce1d3d2f7
Kevin Harper <kevinharperg@gmail.com>
2018-10-07 09:21:05 -0700

    Added admin and tests

--- RESTAPIsWithDjango/05_blog-API/blog_project/posts/tests.py
+++ RESTAPIsWithDjango/05_blog-API/blog_project/posts/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
 
-# Create your tests here.
+from django.contrib.auth.models import User
+
+from .models import Post
+
+
+class BlogTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        testuser1 = User.objects.create_user(
+            username='testuser1', password='abc123')
+        testuser1.save()
+
+        test_post = Post.objects.create(
+            author=testuser1, title='Blog title', body='Body content...')
+        test_post.save()
+
+    def test_blog_content(self):
+        post = Post.objects.get(id=1)
+        expected_author = f'{post.author}'
+        expected_title = f'{post.title}'
+        expected_body = f'{post.body}'
+        self.assertEquals(expected_author, 'testuser1')
+        self.assertEquals(expected_title, 'Blog title')
+        self.assertEquals(expected_body, 'Body content...')
