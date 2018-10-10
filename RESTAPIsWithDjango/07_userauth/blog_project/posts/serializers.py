39dcb62b136505fa025c103b8d2ba576c97f2202
Kevin Harper <kevinharperg@gmail.com>
2018-10-10 10:24:59 -0700

    Added token authentication

---
+++ RESTAPIsWithDjango/07_userauth/blog_project/posts/serializers.py
@@ -0,0 +1,10 @@
+ 
+from rest_framework import serializers
+
+from .models import Post
+
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = ('id', 'author', 'title', 'body', 'created_at')
+        model = Post
