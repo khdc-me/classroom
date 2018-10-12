58c28166004e6ea4e8c5df0ec370616170e3a0ef
Kevin Harper <kevinharperg@gmail.com>
2018-10-11 20:28:27 -0700

    Set up chap 8 project

---
+++ RESTAPIsWithDjango/08_viewsetsandrouters/blog_project/posts/views.py
@@ -0,0 +1,28 @@
+from django.contrib.auth import get_user_model
+
+from rest_framework import generics
+
+from .models import Post
+from .permissions import IsAuthorOrReadOnly
+from .serializers import PostSerializer, UserSerializer
+
+
+class PostList(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthorOrReadOnly,)
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+
+class UserList(generics.ListCreateAPIView):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
---
+++ RESTAPIsWithDjango/08_viewsetsandrouters/config/settings/dev.py
@@ -0,0 +1,14 @@
+from ._base import *
+
+
+ALLOWED_HOSTS = ['*']
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+}
