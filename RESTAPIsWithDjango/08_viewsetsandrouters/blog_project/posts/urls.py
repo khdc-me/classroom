a720c17363f91cc241732ddb73a18d3c601cc189
Kevin Harper <kevinharperg@gmail.com>
2018-10-11 20:45:19 -0700

    Added viewsets & routers

--- RESTAPIsWithDjango/08_viewsetsandrouters/blog_project/posts/urls.py
+++ RESTAPIsWithDjango/08_viewsetsandrouters/blog_project/posts/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
 """
 from django.urls import path
 
-from .views import PostList, PostDetail, UserList, UserDetail
+from rest_framework.routers import SimpleRouter
 
-urlpatterns = [
-    path('users/', UserList.as_view()),
-    path('users/<int:pk>/', UserDetail.as_view()),
-    path('<int:pk>/', PostDetail.as_view()),
-    path('', PostList.as_view()),
-]
+from .views import UserViewSet, PostViewSet
+
+router = SimpleRouter()
+router.register('users', UserViewSet, base_name='users')
+router.register('', PostViewSet, base_name='posts')
+
+urlpatterns = router.urls
