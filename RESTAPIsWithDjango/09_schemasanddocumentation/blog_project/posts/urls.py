df326ad55a19f3bba666dbe6c7cdce388bfd860c
Kevin Harper <kevinharperg@gmail.com>
2018-10-11 21:04:13 -0700

    Added schema

---
+++ RESTAPIsWithDjango/09_schemasanddocumentation/blog_project/posts/urls.py
@@ -0,0 +1,26 @@
+"""blog_project URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.urls import path
+
+from rest_framework.routers import SimpleRouter
+
+from .views import UserViewSet, PostViewSet
+
+router = SimpleRouter()
+router.register('users', UserViewSet, base_name='users')
+router.register('', PostViewSet, base_name='posts')
+
+urlpatterns = router.urls
