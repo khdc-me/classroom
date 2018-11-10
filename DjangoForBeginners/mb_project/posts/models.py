b552d9fa6b1b56e0b0423216f286c7302804c47d
Kevin <kevinharperg@gmail.com>
2018-11-10 12:33:44 -0700

    Added views and templates

--- DjangoForBeginners/mb_project/posts/models.py
+++ DjangoForBeginners/mb_project/posts/models.py
@@ -3,3 +3,7 @@ from django.db import models
 # Create your models here.
 class Post(models.Model):
     text = models.TextField()
+
+    def __str__(self):
+        """String Representation of the model."""
+        return self.text[:50]
