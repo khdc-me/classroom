1ebfa128e97ca382fc6038a55bf2d6202e826f6a
Kevin <kevinharperg@gmail.com>
2018-11-11 11:33:07 -0700

    Added post model test

--- DjangoForBeginners/ch05/blog_project/blog/tests.py
+++ DjangoForBeginners/ch05/blog_project/blog/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
 
+from .models import Post
+
 # Create your tests here.
+class PostModelTest(TestCase):
+    def setUp(self):
+        Post.objects.create(text="This is a test.")
+
+    def test_text_content(self):
+        post = Post.objects.get(id=1)
+        expected_object_name = f'{post.text}'
+        self.assertEqual(expected_object_name, 'This is a test.')
