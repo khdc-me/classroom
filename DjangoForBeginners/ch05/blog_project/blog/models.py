a6f2d893dcc5d27d99d74f05b9782fc957b544c9
Kevin <kevinharperg@gmail.com>
2018-11-11 13:11:52 -0700

    Created models

--- DjangoForBeginners/ch05/blog_project/blog/models.py
+++ DjangoForBeginners/ch05/blog_project/blog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    body = models.TextField()
+    author = models.ForeignKey(
+        'auth.User',
+        on_delete = models.CASCADE,
+    )
+    
+    def __str__(self):
+        """ String representation of the model. """
+        return self.title
