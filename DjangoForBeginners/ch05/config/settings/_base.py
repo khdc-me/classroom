d6f46195bd2a43e9e85febfff7c4ecaeda3b1e0f
Kevin <kevinharperg@gmail.com>
2018-11-11 11:22:55 -0700

    Created base blog app

--- DjangoForBeginners/ch05/config/settings/_base.py
+++ DjangoForBeginners/ch05/config/settings/_base.py
@@ -47,12 +47,16 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # defatuls apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # personal apps
+    'blog_project.blog',
 ]
 
 MIDDLEWARE = [
