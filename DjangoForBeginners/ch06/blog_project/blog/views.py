66360042a4a4417ec1a1687973a4fca75fa22650
Kevin Harper <kevinharperg@gmail.com>
2018-11-13 18:44:24 -0700

    Added functionality to delete a post

--- DjangoForBeginners/ch06/blog_project/blog/views.py
+++ DjangoForBeginners/ch06/blog_project/blog/views.py
@@ -1,5 +1,7 @@
 from django.views.generic import ListView, DetailView
-from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+
+from django.urls import reverse_lazy
 
 from .models import Post
 
@@ -24,3 +26,9 @@ class BlogUpdateView(UpdateView):
 	model = Post
 	fields = ['title', 'body']
 	template_name = 'post_edit.html'
+
+
+class BlogDeleteView(DeleteView):
+	model = Post
+	template_name = 'post_delete.html'
+	success_url = reverse_lazy('home')
